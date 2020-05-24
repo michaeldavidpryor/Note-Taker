659ce4347450c97e3b7357275524f6298e64644f
michaeldavidpryor <mdpcaps@gmail.com>
2020-05-23 23:55:48 -0400

    removed the static route from public file paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,11 +4,11 @@ const path = require('path')
 module.exports = function (app) {
 
     app.get("/notes", function (rew, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "/notes.html"));
     });
 
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "/index.html"));
     });
 };
 
--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const path = require('path')
 const port = process.env.PORT || 3000;
 const chalk = require('chalk');
 const express = require('express');
+const http = require('http');
 const app = express();
-const debug = require ('debug')('app')
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+
 require('./routes/apiRoutes.js')(app);
 require('./routes/htmlRoutes.js')(app);
 
+// const server = http.createServer((req, res) => {
+//    res.statusCode = 200;
+//    res.setHeader('Content-Type', 'text/html');
+//    res.end('<h1>Note Taker</h1>');
+// });
+
 app.listen(port, () => {
-   debug(`listening on port ${chalk.cyanBright(port)}`);
+   console.log(`listening on port ${chalk.cyanBright(port)}`);
 });
 
