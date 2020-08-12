c55b6b1d2effa1fd3768bb9d1063b5e0798bc7f5
Michael Pryor <mdpcaps@gmail.com>
2020-08-12 19:57:50 -0400

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "/notes.html"));
     });
 
-    app.get('*', function (req, res) {
+    app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, "/index.html"));
     });
 };
